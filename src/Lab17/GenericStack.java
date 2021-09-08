c6225abcc1fcf3e15ba74ec7f3d2bb70d4ba8dd7
BushraOnika <bushrahossainonika@gmail.com>
2021-09-08 16:52:59 +0600

    Messy codes messy meeee

---
+++ src/Lab17/GenericStack.java
@@ -0,0 +1,13 @@
+package Lab17;
+
+import java.util.ArrayList;
+
+public class GenericStack<E> {
+    private ArrayList<E> list;
+    private Object E;
+
+    public int getSize(){
+        return list.size();
+    }
+
+}
