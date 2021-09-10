820c175eec343133f9192d59014945777dfbd30a
BushraOnika <bushrahossainonika@gmail.com>
2021-09-10 15:41:32 +0600

    Me only for meee

--- src/Lab17/GenericStack.java
+++ src/Lab17/GenericStack.java
@@ -1,13 +1,21 @@
 package Lab17;
-
 import java.util.ArrayList;
+public class GenericStack<T> {
+    private ArrayList<T> list;
 
-public class GenericStack<E> {
-    private ArrayList<E> list;
-    private Object E;
-
+    public GenericStack() {
+       list = new ArrayList<T>();
+    }
     public int getSize(){
         return list.size();
+    }
+    public T peek(){
+        return list.get(getSize()-1);
+    }
+    public T pop(){
+        T item = peek();
+        list.remove(getSize()-1);
+        return item;
     }
 
 }
